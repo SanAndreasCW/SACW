0cf4343a805188b09784569e6e2804b9965f081f
mahmoodreza <mahmodreza.mr@gmail.com>
2025-01-16 18:46:51 +0330

    feat: Type conversion methods

--- mod/commons/commons.go
+++ mod/commons/commons.go
@@ -1,1 +1,19 @@
 package commons
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func IntToString[T int | int32 | int64 | int16 | int8](i T) string {
+	return fmt.Sprintf("%d", i)
+}
+
+func StringToInt[T ~int | ~int64 | ~int32 | ~int16 | ~int8](s *string) (T, error) {
+	parsed, err := strconv.Atoi(*s)
+	if err != nil {
+		panic(err)
+		return T(0), err
+	}
+	return T(parsed), nil
+}
