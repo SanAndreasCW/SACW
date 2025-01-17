2a6667244d046dc3d13db376a2b0ec190f230757
mahmoodreza <mahmodreza.mr@gmail.com>
2025-01-17 13:17:59 +0330

    feat: Type conversion functions

--- mod/commons/commons.go
+++ mod/commons/commons.go
@@ -9,10 +9,22 @@ func IntToString[T int | int32 | int64 | int16 | int8](i T) string {
 	return fmt.Sprintf("%d", i)
 }
 
+func FloatToString[T float32 | float64](f T) string {
+	return fmt.Sprintf("%f", f)
+}
+
 func StringToInt[T ~int | ~int64 | ~int32 | ~int16 | ~int8](s *string) (T, error) {
 	parsed, err := strconv.Atoi(*s)
 	if err != nil {
 		panic(err)
+		return T(0), err
+	}
+	return T(parsed), nil
+}
+
+func StringToFloat[T ~float32 | ~float64](s *string) (T, error) {
+	parsed, err := strconv.ParseFloat(*s, 64)
+	if err != nil {
 		return T(0), err
 	}
 	return T(parsed), nil
