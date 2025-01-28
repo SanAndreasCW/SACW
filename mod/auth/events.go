1f6ffbdbe2d97a6c782dd1273eb61d903efd482b
mahmoodreza <mahmodreza.mr@gmail.com>
2025-01-28 17:59:29 +0330

    fix: Auth event handler

--- mod/auth/events.go
+++ mod/auth/events.go
@@ -2,12 +2,11 @@ package auth
 
 import (
 	"github.com/SanAndreasCW/SACW/mod/commons"
-	"github.com/kodeyeen/event"
 	"github.com/kodeyeen/omp"
 )
 
 var (
-	Events = event.NewDispatcher()
+	Events = omp.NewDispatcher()
 )
 
 const (
