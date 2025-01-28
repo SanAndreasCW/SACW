60c176903ca0f0bdf11551445a320dcc13cdff76
mahmoodreza <mahmodreza.mr@gmail.com>
2025-01-28 16:59:06 +0330

    fix: Colors

--- mod/colors/hex.go
+++ mod/colors/hex.go
@@ -1,12 +1,10 @@
 package colors
 
-import "github.com/kodeyeen/omp"
-
 const (
-	WhiteHex   omp.Color = 0xFFFFFFFF
-	InfoHex    omp.Color = 0xf1c40fFF
-	NoteHex    omp.Color = 0xf39c12FF
-	SuccessHex omp.Color = 0x2ecc71FF
-	ErrorHex   omp.Color = 0xe74c3cFF
-	NoticeHex  omp.Color = 0x3498dbFF
+	WhiteHex   uint = 0xFFFFFFFF
+	InfoHex    uint = 0xf1c40fFF
+	NoteHex    uint = 0xf39c12FF
+	SuccessHex uint = 0x2ecc71FF
+	ErrorHex   uint = 0xe74c3cFF
+	NoticeHex  uint = 0x3498dbFF
 )
