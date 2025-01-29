377e375107be7ef2ee88ced47782cf556ef877a0
mahmoodreza <mahmodreza.mr@gmail.com>
2025-01-29 14:22:19 +0330

    fix: Added more int types to conversions

--- mod/commons/commons.go
+++ mod/commons/commons.go
@@ -5,7 +5,7 @@ import (
 	"strconv"
 )
 
-func IntToString[T int | int32 | int64 | int16 | int8](i T) string {
+func IntToString[T int | int32 | int64 | int16 | int8 | uint | uint32 | uint64 | uint16 | uint8](i T) string {
 	return fmt.Sprintf("%d", i)
 }
 
@@ -13,10 +13,9 @@ func FloatToString[T float32 | float64](f T) string {
 	return fmt.Sprintf("%f", f)
 }
 
-func StringToInt[T ~int | ~int64 | ~int32 | ~int16 | ~int8](s *string) (T, error) {
+func StringToInt[T ~int | ~int64 | ~int32 | ~int16 | ~int8 | ~uint | ~uint64 | ~uint32 | ~uint16 | ~uint8](s *string) (T, error) {
 	parsed, err := strconv.Atoi(*s)
 	if err != nil {
-		panic(err)
 		return T(0), err
 	}
 	return T(parsed), nil
@@ -25,7 +24,6 @@ func StringToInt[T ~int | ~int64 | ~int32 | ~int16 | ~int8](s *string) (T, error
 func StringToFloat[T ~float32 | ~float64](s *string) (T, error) {
 	parsed, err := strconv.ParseFloat(*s, 64)
 	if err != nil {
-		panic(err)
 		return T(0), err
 	}
 	return T(parsed), nil
