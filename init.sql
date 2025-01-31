bd9f3c6ee215793ee58ecbfdaf72697ad83d6a7f
mahmoodreza <mahmodreza.mr@gmail.com>
2025-01-31 13:13:36 +0330

    feat: Initializer query file

---
+++ init.sql
@@ -0,0 +1,33 @@
+-- Digkala
+INSERT INTO company
+VALUES (1,
+        'Digikala',
+        'DK',
+        'Behtarin foroshgah mojod dar SanAndreas.',
+        0,
+        1.0);
+INSERT INTO company_office
+VALUES (DEFAULT,
+        1,
+        2183.459717,
+        -2254.026855,
+        2183.459717,
+        -2254.026855,
+        14.771435);
+
+-- Snapp
+INSERT INTO company
+VALUES (2,
+        'Snapp',
+        'SNP',
+        'Behtarin taxi online dar SanAndreas.',
+        0,
+        1.0);
+INSERT INTO company_office
+VALUES (DEFAULT,
+        2,
+        1500.237793,
+        -1053.283936,
+        1500.237793,
+        -1053.283936,
+        25.062500);
